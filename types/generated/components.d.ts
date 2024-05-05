a153697a492f7c61f280e7eaedd4028d7eb7fd9c
abr-ya <gamer05@list.ru>
2024-05-05 21:53:57 +0300

    feat: Add Order type

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,26 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface OrderOrderItem extends Schema.Component {
+  collectionName: 'components_order_order_items';
+  info: {
+    displayName: 'OrderItem';
+    icon: 'archive';
+  };
+  attributes: {
+    product: Attribute.Relation<
+      'order.order-item',
+      'oneToOne',
+      'api::product.product'
+    >;
+    price: Attribute.Integer;
+    quantity: Attribute.Integer;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'order.order-item': OrderOrderItem;
+    }
+  }
 }
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -836,6 +836,44 @@ export interface ApiCategoryCategory extends Schema.CollectionType {
   };
 }
 
+export interface ApiOrderOrder extends Schema.CollectionType {
+  collectionName: 'orders';
+  info: {
+    singularName: 'order';
+    pluralName: 'orders';
+    displayName: 'Order';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    user_adress: Attribute.Relation<
+      'api::order.order',
+      'oneToOne',
+      'api::user-adress.user-adress'
+    >;
+    total: Attribute.Integer;
+    userId: Attribute.Integer;
+    paymentId: Attribute.String;
+    orderItemList: Attribute.Component<'order.order-item', true>;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::order.order',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::order.order',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface ApiProductProduct extends Schema.CollectionType {
   collectionName: 'products';
   info: {
@@ -1016,6 +1054,7 @@ declare module '@strapi/types' {
       'plugin::users-permissions.user': PluginUsersPermissionsUser;
       'plugin::i18n.locale': PluginI18NLocale;
       'api::category.category': ApiCategoryCategory;
+      'api::order.order': ApiOrderOrder;
       'api::product.product': ApiProductProduct;
       'api::slider.slider': ApiSliderSlider;
       'api::user-adress.user-adress': ApiUserAdressUserAdress;
